5f2e11e5d8a6177fae79bf50de8498700001f750
allexna <alexna39@gmail.com>
2020-02-02 21:49:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication19/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myapplication19;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        TextView infoTextView=findViewById(R.id.textView);
+
+
+        switch (id){
+            case R.id.action_cat1:
+                infoTextView.setText("Вы выбрали кота!");
+                return true;
+            case R.id.action_cat2:
+                infoTextView.setText("Вы выбрали кошку!!");
+                return true;
+            case R.id.action_cat3:
+                infoTextView.setText("Вы выбрали котенка!");
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
